8fc9fd91ecdfd50761720b628f89a523888f1550
LoickChd <loick.chaudefosse@etu.univ-poitiers.fr>
2023-12-06 18:01:47 +0100

    comparaCharactersPage sans graph

--- src/pages/CompareCharactersPage.js
+++ src/pages/CompareCharactersPage.js
@@ -1,17 +1,16 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { getCharacters } from '../api/character-api';
+import { PureComponent } from 'react';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
+
 
 const CompareCharactersPage = () => {
     // change the title of the page
     document.title = "Compare | Marvel App";
 
     // to be replaced with the real data
-    const characters = [
-        {
-            name: '...'
-        },{
-            name: '...'
-        }
-    ]
+    const [characters, setCharacters] = useState([]);
 
     // transform the characters to array of label/value objects
     const options = characters.map((character, index) => ({
@@ -28,39 +27,61 @@ const CompareCharactersPage = () => {
         width: 500,
     };
 
+    useEffect(() => {
+        const characters = getCharacters();
+        setCharacters(characters);
+        setOption1(characters[0]);
+        setOption2(characters[1]);
+    }, []);
+
     return (
         <>
             <h2>Compare characters</h2>
 
             <p style={centerStyle}>
-                <select
-                    value={option1.value}
-                    onChange={(event) => setOption1(options[event.target.value])}
-                >
-                    {options.map((option) => (
-                        <option key={option.value} value={option.value}>
-                            {option.label}
+            <select
+                value={option1?.id}
+                onChange={(event) => setOption1(characters.find(character => character.id === event.target.value))}
+>
+                    {characters.map((character) => (
+                        <option key={character.id} value={character.id}>
+                            {character.name}
                         </option>
                     ))}
                 </select>&nbsp; {/* Fix the ambiguous spacing */}
                 with&nbsp;
                 <select
-                    value={option2.value}
-                    onChange={(event) => setOption2(options[event.target.value])}
-                >
-                    {options.map((option) => (
-                        <option key={option.value} value={option.value}>
-                            {option.label}
+                    value={option2?.id}
+                    onChange={(event) => setOption2(characters.find(character => character.id === event.target.value))}
+>
+                    {characters.map((character) => (
+                        <option key={character.id} value={character.id}>
+                            {character.name}
                         </option>
                     ))}
                 </select>
             </p>
 
             <p style={centerStyle}>
-                {characters[option1.value].name} vs {characters[option2.value].name}
+            <img src={`${option1?.thumbnail.path}.jpg`} alt={option1?.name} style={{width: "150px"}} /> vs <img src={`${option2?.thumbnail.path}.jpg`} alt={option2?.name} style={{width: "150px"}} />
             </p>
+            <p style={centerStyle}>
+            {option1?.name} vs {option2?.name}
+            </p>
+
+
+                
+            <RadarChart outerRadius={90} width={730} height={250} data={characters}>
+            <PolarGrid />
+            <PolarAngleAxis dataKey="capacities" />
+            <PolarRadiusAxis angle={30} domain={[0, 150]} />
+            <Radar name={option1?.name} dataKey={option1?.force} stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+            <Radar name={option2?.name} dataKey={option2?.force} stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+            </RadarChart>
+
         </>
     );
+ 
 };
 
 export default CompareCharactersPage;
